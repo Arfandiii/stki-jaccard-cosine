cf77140bd9a3c396bf2b8ae523a9db1865e1e241
Arfandiii <muhammadari280702@gmail.com>
2025-10-31 19:52:55 +0700

    commit 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
                 <label class="block text-sm font-medium text-gray-700 mb-2">Masukkan kata kunci pencarian</label>
                 <div class="flex gap-2">
                     <input type="text" id="searchInput" placeholder="Cari surat masuk/keluar..." 
-                           class="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent">
+                            class="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent">
                     <button onclick="performSearch()" 
                             class="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200">
                         <i class="fas fa-search mr-2"></i>Cari
